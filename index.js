f96aaf066004539f1ea699763e94d302ceef337d
Caz <cazyw@users.noreply.github.com>
2018-01-07 01:04:29 +1100

    Learn how to chain promises so synchronous retrieval of playlists

--- index.js
+++ index.js
@@ -1,28 +1,98 @@
-console.log('starting');
+console.log('starting console log');
+let userPlaylists = [];
+let noPlaylists = 0;
 var SpotifyWebApi = require('spotify-web-api-js');
+var q = require('Q');
+var promises = [];
 
 var spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken('fill in');
 
-spotifyApi.getUserPlaylists('elliedub')
-  .then(function(data) {
-    console.log('User playlists', data, data.total);
-    let loops = data.total / 20;
-    console.log(`loops: ${loops}`);
-    for(var i = 0; i < (data.total) ; i += 20){
-      console.log('test', i);
-      spotifyApi.getUserPlaylists('elliedub', {offset: i})
-      .then(function(data){
-        console.log(data);
-      }, function(err){
-        console.error(err);
-      });
-    }
-    console.log('hi');
-  }, function(err) {
-    console.error(err);
-});
+function doCalls(message) {
+  var step = 0;
+  console.log(message);
+  return new Promise(function(resolve, reject) {
+      resolve(1);
+  })
+  .then(function (result) {
+    return Promise.resolve(
+      spotifyApi.getUserPlaylists('elliedub')
+        .then(function(data) {
+          console.log(`== Resolve ${++step} ==`);
+          console.log(`Number of playlists: ${data.total}, ${Math.ceil(data.total / 20)} loops`)
+          noPlaylists = data.total
+        }, function(err){
+          console.error(err);
+        })
+    )
+  })
+  .then(function (result) {
+    return Promise.resolve(
+      getNames(++step)
+    )
+  })
+      .then(function (result) {
+          return Promise.resolve(
+              // rp(req3, function (err, resp) {
+                  console.log('step: ', ++step, ' UHub')
+              // })
+          )
+      })
+      .then(function (result) {
+          console.log('Ending demo ');
+          return Promise.resolve(step);
+      })
+}
 
+doCalls('Starting calls')
+  .then(function (steps) {
+          console.log('Completed in step ', steps);
+  });
+
+
+// spotifyApi.getUserPlaylists('elliedub')
+//   .then(function(data) {
+//     console.log('User playlists', data, data.total);
+//     let loops = data.total / 20;
+//     console.log(`loops: ${loops}`);
+//     return noPlaylists = data.total;
+//   }, function(err){
+//     console.error(err);
+//   })
+//   .then(function(){
+//     getNames();
+//   })
+//   .then(printEnd);
+
+// function printEnd(){
+//   console.log('end');
+// }
+
+function getNames(step) {
+  console.log(`== Resolve ${step} ==`);
+  var promises = [];
+  for(var i = 0; i < (noPlaylists) ; i += 20){
+    promises.push(spotifyApi.getUserPlaylists('elliedub', {offset: i})
+    .then(function(data){
+      console.log(data.items);
+      const playlists = data.items;
+      playlists.forEach(function(playlist) {
+        userPlaylists.push({name:playlist.name, id:playlist.id});
+      });
+    })
+    .catch((e) => {
+      // handle errors here
+      console.error(e);
+    })
+    )
+  }
+  return Promise.all(promises)
+  .then('finished')
+  .catch((e) => {
+    // handle errors here
+    console.error(e);
+  });
+}
 
 // spotifyApi.getUserPlaylists('elliedub', {offset: i})
 //         .then(function(data) {
