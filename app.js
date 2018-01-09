42ea6d14c95c7be55ef7d218704111fa02e493ae
Caz <cazyw@users.noreply.github.com>
2018-01-09 22:25:40 +1100

    Update app.js to force the user to approve the app again if they’ve already done so as this also gives an option to log out

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ app.get('/login', function(req, res) {
       client_id: client_id,
       // scope: scope,
       redirect_uri: redirect_uri,
-      state: state
+      state: state,
+      show_dialog: true
     }));
 });
 
