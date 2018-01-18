acc65578bdf2cb2bc151fcdb98deca5e6003caef
Caz <cazyw@users.noreply.github.com>
2018-01-18 23:42:54 +1100

    No changes

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
-
+   
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
